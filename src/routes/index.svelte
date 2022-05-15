64e98cde2902da2daacfb927249e5e769229e1c0
iiiivi <104396465+iiiivi@users.noreply.github.com>
2022-05-14 18:38:04 -0700

    link fix
    Index page- template 2- button link fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -73,7 +73,7 @@
           </div>
           <div class="p-6">
             <a
-              href="/template3"
+              href="/template2"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
               class="inline-block px-6 py-2.5 bg-slate-800 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
